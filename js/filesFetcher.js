1c77dc001f737e3c0f4e1590b3f0bb000f87ff35
Linvin_ <weizhihan3@gmail.com>
2025-07-17 13:56:20 +0200

    Update filesFetcher.js

--- js/filesFetcher.js
+++ js/filesFetcher.js
@@ -1,4 +1,4 @@
-const API_URL = "https://github-schema.weizhihan3.workers.dev/contents/contents/schematic";
+const API_URL = "https://openst.weizhihan3.workers.dev/contents/contents/schematic";
 
 let files = [];
 
